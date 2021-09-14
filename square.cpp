24ce4e3569ce9c69ea8677fa972d8495ea82376f
Emil Tsanev <emiltsanev2001@gmail.com>
2021-09-14 12:31:47 +0300

    Изчистена версия

--- square.cpp
+++ square.cpp
@@ -4,23 +4,17 @@
 #include <algorithm>
 using namespace std;
 
-void first_version();
-void second_version();
 
 int main()
 {
-	second_version();
-}
-
-void second_version() {
 	int a;
 	cin >> a;
 
 	int max = 0, counter = 0;
 	vector<int> lines;
 	for (int i = 1; i <= a; i++) {
-		for (int j = 1; j <= a; j++) {
-			long x = i*i+j*j;
+		for (int j = 1; j < i; j++) {
+			long x = i * i + j * j;
 			long double len = sqrt(x);
 			if (len - floor(len) == 0) {
 				if (max < len) {
@@ -37,35 +31,4 @@ void second_version() {
 	cout << max << " " << counter;
 }
 
-void first_version() {
-	int a;
-	cin >> a;
 
-	int max = 0, counter = 0;
-	vector<int> lines;
-	for (int i = 0; i <= a; i++) {
-		for (int j = 0; j < i; j++) {
-			for (int k = 0; k <= a; k++) {
-				if (k != i) {
-					for (int l = 0; l < k; l++) {
-						if (l != j) {
-							long x = (abs(i - k) * abs(i - k)) + (abs(j - l) * abs(j - l));
-							long double len = sqrt(x);
-							if (len - floor(len) == 0) {
-								if (max < len) {
-									max = len;
-								}
-								if (!count(lines.begin(), lines.end(), len)) {
-									counter++;
-									lines.push_back(len);
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-
-	cout << max << " " << counter;
-}
