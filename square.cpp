2c382926c6c4c004ce03ba855e53de10a25203df
Emil Tsanev <emiltsanev2001@gmail.com>
2021-09-14 11:32:41 +0300

    Initial commit

---
+++ square.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int a;
+	cin >> a;
+
+	int max = 0, counter = 0;
+	vector<int> lines;
+	for (int i = 0; i <= a; i++) {
+		for (int j = 0; j <= i; j++) {
+			for (int k = 0; k <= a; k++) {
+				if (k != i) {
+					for (int l = 0; l <= a; l++) {
+						if (l != j) {
+							long x = (abs(i - k) * abs(i - k)) + (abs(j - l) * abs(j - l));
+							long double len = sqrt(x);
+							if (len - floor(len) == 0) {
+								if (max < len) {
+									max = len;
+								}
+								if (!count(lines.begin(), lines.end(), len)) {
+									counter++;
+									lines.push_back(len);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	cout << max << " " << counter;
+}
+
